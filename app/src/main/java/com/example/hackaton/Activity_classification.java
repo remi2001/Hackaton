c15075ef5f1829e7fd37e1212afba364da67ec28
AlbanMayeux <alban.mayeux@orange.fr>
2022-03-11 16:13:19 +0100

    changement page choix produit

--- app/src/main/java/com/example/hackaton/Activity_classification.java
+++ app/src/main/java/com/example/hackaton/Activity_classification.java
@@ -6,7 +6,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -33,16 +32,17 @@ public class Activity_classification extends AppCompatActivity {
 
 
         invalidButton.setOnClickListener(view-> onClick());
-        validButton.setOnClickListener(view-> onClick());
 
 
     }
 
     private void onClick() {
-        ImageView imageViewVar = (ImageView) findViewById(R.id.imageView);
-        int [] image={R.drawable.y1,R.drawable.y2,R.drawable.y3,R.drawable.y4,R.drawable.y5,R.drawable.y6,R.drawable.y7,R.drawable.y8,R.drawable.y9,R.drawable.y10,R.drawable.y11,R.drawable.y12,R.drawable.y13,R.drawable.y14,R.drawable.y15};
-        imageViewVar.setImageResource(image[numeroImage]);
+        String chemin="../../res/drawable/y"; //mettre le chemin des photos ...../yaourt_
+        chemin+=numeroImage+".png";
+        File imgFile= new File(chemin);
         numeroImage++;
+        Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getPath());
+        this.imageView.setImageBitmap(myBitmap);
     }
 
 
