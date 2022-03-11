f4d17b886ece1f4cf7096b685375d7da546bc574
d-oceane <78603636+d-oceane@users.noreply.github.com>
2022-03-11 16:36:13 +0100

    correction onClick

--- app/src/main/java/com/example/hackaton/Activity_classification.java
+++ app/src/main/java/com/example/hackaton/Activity_classification.java
@@ -37,12 +37,10 @@ public class Activity_classification extends AppCompatActivity {
     }
 
     private void onClick() {
-        String chemin="../../res/drawable/y"; //mettre le chemin des photos ...../yaourt_
-        chemin+=numeroImage+".png";
-        File imgFile= new File(chemin);
+        ImageView imageViewVar = (ImageView) findViewById(R.id.imageView);
+        image= new int[]{R.drawable.y1, R.drawable.y2, R.drawable.y3, R.drawable.y4, R.drawable.y5, R.drawable.y6, R.drawable.y7, R.drawable.y8, R.drawable.y9, R.drawable.y10, R.drawable.y11, R.drawable.y12, R.drawable.y13, R.drawable.y14, R.drawable.y15};
+        imageViewVar.setImageResource(tableau[numeroImage]);
         numeroImage++;
-        Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getPath());
-        this.imageView.setImageBitmap(myBitmap);
     }
 
 
