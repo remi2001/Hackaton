be2e284de8e18f11e4d9d419e8d557f7bf5db190
elodie.blairet@etud.u-picardie.fr <elodie.blairet@etud.u-picardie.fr>
2022-03-11 13:26:02 +0100

    boutons choix

--- app/src/main/java/com/example/hackaton/Activity_choix_produit.java
+++ app/src/main/java/com/example/hackaton/Activity_choix_produit.java
@@ -2,7 +2,9 @@ package com.example.hackaton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Activity_choix_produit extends AppCompatActivity {
 
@@ -10,6 +12,26 @@ public class Activity_choix_produit extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choix_produit);
-        //commentaire
+
+        //Ajout des boutons
+        Button yaourtButton = findViewById(R.id.button_yaourt);
+        Button bouteilleButton = findViewById(R.id.button_bouteille);
+        Button accueilButton = findViewById(R.id.button_accueil);
+
+        accueilButton.setOnClickListener(view -> openhomeActivity());
+        yaourtButton.setOnClickListener(view -> openclassificationActivity());
+        bouteilleButton.setOnClickListener(view -> openclassificationActivity());
+    }
+
+    //ouvre la page home
+    private void openhomeActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    //ouvre la page de classification
+    private void openclassificationActivity(){
+        Intent intent = new Intent(this, Activity_classification.class);
+        startActivity(intent);
     }
 }
