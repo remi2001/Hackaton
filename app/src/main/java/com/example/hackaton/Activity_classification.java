aed34ce75a11c9acadcdea74774410a2a09c0ac4
oceane-dassou <oceane.dassou@outlook.fr>
2022-03-11 16:53:11 +0100

    correction onClick()

--- app/src/main/java/com/example/hackaton/Activity_classification.java
+++ app/src/main/java/com/example/hackaton/Activity_classification.java
@@ -37,16 +37,16 @@ public class Activity_classification extends AppCompatActivity {
         //if(produit==1)
             //image= new int[]{R.drawable.y1, R.drawable.y2, R.drawable.y3, R.drawable.y4, R.drawable.y5, R.drawable.y6, R.drawable.y7, R.drawable.y8, R.drawable.y9, R.drawable.y10, R.drawable.y11, R.drawable.y12, R.drawable.y13, R.drawable.y14, R.drawable.y15};
 
-        invalidButton.setOnClickListener(view-> onClick(image));
-        validButton.setOnClickListener(view-> onClick(image));
+        invalidButton.setOnClickListener(view-> onClick());
+        validButton.setOnClickListener(view-> onClick());
 
 
     }
 
-    private void onClick(int[] tableau) {
+    private void onClick() {
         ImageView imageViewVar = (ImageView) findViewById(R.id.imageView);
         image= new int[]{R.drawable.y1, R.drawable.y2, R.drawable.y3, R.drawable.y4, R.drawable.y5, R.drawable.y6, R.drawable.y7, R.drawable.y8, R.drawable.y9, R.drawable.y10, R.drawable.y11, R.drawable.y12, R.drawable.y13, R.drawable.y14, R.drawable.y15};
-        imageViewVar.setImageResource(tableau[numeroImage]);
+        imageViewVar.setImageResource(image[numeroImage]);
         numeroImage++;
     }
 
