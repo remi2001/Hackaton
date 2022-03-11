b959e92b9e374a17d20aeddb4b0c73f5cfa8349b
AlbanMayeux <alban.mayeux@orange.fr>
2022-03-11 13:10:39 +0100

    bouton page final

--- app/src/main/java/com/example/hackaton/Final_page.java
+++ app/src/main/java/com/example/hackaton/Final_page.java
@@ -2,7 +2,9 @@ package com.example.hackaton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Final_page extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class Final_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_final_page);
+
+        Button Bouttonretouraccueil = findViewById(R.id.Button_final_page);
+        Bouttonretouraccueil.setOnClickListener(view -> revenirAccueil());
+    }
+
+    private void revenirAccueil(){
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
     }
 }
